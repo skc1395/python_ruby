f5e08913cfd81bed46797075274a9c3cef4ac01a
GiChang <skc1395@naver.com>
2018-01-31 14:55:04 +0900

    Add files via upload

--- 1.rb
+++ 1.rb
@@ -1,5 +1,3 @@
-puts(1==1)
-puts(1==2)
-puts(1<2)
-puts(true)
-puts(false)
+if true
+  puts("Hello")
+end
--- 2.py
+++ 2.py
@@ -1,4 +1,6 @@
-name = "최진혁"
-print("안녕하세요"+name+"님")
-print(name+"님을 위한 "+"강의를 준비했습니다.")
-print(name+"님 꼭 참석부탁드립니다.")
+input = 11
+real = 11
+if real == input:
+    print("Hello!")
+else:
+    print("Who are you?")
